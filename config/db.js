541df1594a103691beb6acfa2b5b65c87e551f63
sergio7174 <sergiogmoncada@yahoo.com>
2024-10-08 15:44:35 -0400

    first commit

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const
+mongoose = require("mongoose");
+
+module.exports = {
+
+ connectDb : () => {
+
+const URL = process.env.MONGO_URL;
+console.log("Estoy en db - line 9 - process.env.MONGO_URL: "+process.env.MONGO_URL)
+// Add Mongoose connection to Express.js
+mongoose.connect(
+    // Set up the connection to your database.  
+    URL,
+    
+     
+      {// useNewUrlParser: true , // not longer neccesary
+        // useFindAndModify: false } // not longer neccesary
+      });
+    //mongoose.set("useCreateIndex", true); // not longer neccesary
+    // Assign the database to the db variable.
+    const db = mongoose.connection;
+    
+    // Log a message when the application connects to the database.
+    db.once("open", () => {
+      console.log("Successfully connected to MongoDB using Mongoose!");
+    });
+}}
---
+++ controller/userController.js
@@ -0,0 +1,108 @@
+"use strict";
+
+const 
+  
+  User = require("../models/userModel"),
+  jwt= require("jsonwebtoken"),
+  bcrypt = require("bcrypt"),
+  UserController = {};
+
+// Export object literal with all controller actions.
+
+UserController.createUser = async (req, res) => {
+
+        const { name, email, password, avatarUrl } = req.body;
+        console.log("Estoy en userController - line 18");
+        console.log("Estoy en userController - line 19 - name: "+name);
+        console.log("Estoy en userController - line 20 - pass: "+password);
+        
+        if (!name || !email || !password) {
+          res.status(400).json({ error: "Please fill all fields" });
+          return;
+        }
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+          res.status(400).json({ error: "User already exists" });
+          return;
+        }
+        console.log("Estoy en userController - line 31: ");
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        console.log("Estoy en userController - line 33, salt: "+salt);
+        
+        console.log("Estoy en userController - line 34, process.env.JWT_ACCESS_TOKEN: "+process.env.JWT_ACCESS_TOKEN)
+        
+        const newUser = new User({
+          name,
+          email,
+          password: hashedPassword,
+          avatarUrl,
+        });
+        const createdUser = await newUser.save();
+        const token = jwt.sign(
+          { userId: createdUser._id },
+          process.env.JWT_ACCESS_TOKEN,
+          {
+            expiresIn: "5d",
+          }
+        );
+        if (createdUser) {
+          res.status(200).json({
+            _id: createdUser.id,
+            name: createdUser.name,
+            email: createdUser.email,
+            token,
+          });
+        } else {
+          res.status(400).json({ error: "Ivalid user data" });
+          return;
+        }
+    },
+
+// @desc Login user
+// @route POST /users/login
+// @access Public
+
+
+
+UserController.loginUser= async (req, res, next) => {
+
+  console.log("EStoy en userController - line 67 - loginUser")
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({ error: "Please fill all fields" });
+      return;
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401).json({ error: "Invalid email or password" });
+      console.log("EStoy en userController - line 74 - error: No user")
+      return;
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+  
+    if (!isMatch) {
+      res.status(401).json({ error: "Invalid email or password" });
+      console.log("EStoy en userController - line 80 - error: No Match")
+      return;
+    }
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_ACCESS_TOKEN ,
+      {
+        expiresIn: "5d",
+      }
+    );
+  
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+  }
+
+
+
+  module.exports = UserController;
